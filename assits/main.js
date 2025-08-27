3bc85e1e589d58439e0ae5bc0fccb4d5f04017f8
amjad-AR <amjadalaranji18333@gmail.com>
2025-08-27 12:20:40 +0300

    summing && prime

--- assits/main.js
+++ assits/main.js
@@ -1,13 +1,36 @@
-let x = 5;
-let y = 4;
-let z = 8;
-let max;
-if (x > y && x > y) {
-    max = x
-} else if (y > x && y > z) {
-    max = y;
-} else {
-    max = z
-}
+// let x = 5;
+// let y = 4;
+// let z = 8;
+// let max;
+// if (x > y && x > y) {
+//     max = x
+// } else if (y > x && y > z) {
+//     max = y;
+// } else {
+//     max = z
+// }
 
-console.log(max)
+// console.log(max)
+
+function summing(num) {
+    if (num % 2 == 1) {
+        // let num1 = num;
+        if (num > 0) {
+            let i = 0;
+            let num2 = 0;
+            while (i < num) {
+                num2 += num2 + 1;
+                if (i == num) {
+                    break
+                }
+            }
+            return num2
+        } else {
+            return 0;
+
+        }
+    } else {
+        console.log("this number is not Prime number")
+    }
+}
+console.log(summing(5));
