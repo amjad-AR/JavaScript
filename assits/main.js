d8b2b7b46736a2fc06226caf0c90de4a1241b378
amjad-AR <amjadalaranji18333@gmail.com>
2025-08-27 11:10:16 +0300

    maxNumber

---
+++ assits/main.js
@@ -0,0 +1,17 @@
+let x = 5;
+let y = 4;
+let z = 8;
+
+if (x > y) {
+    if (x < z) {
+        console.log(z)
+    } else {
+        console.log(x)
+    }
+} else {
+    if (y < z) {
+        console.log(z)
+    } else {
+        console.log(y)
+    }
+}
