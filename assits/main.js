2d8faf79e4428f6328e886b2a8a1ace577c4ee06
amjad-AR <amjadalaranji18333@gmail.com>
2025-09-04 15:58:59 +0300

    New Question

--- assits/main.js
+++ assits/main.js
@@ -12,25 +12,135 @@
 
 // console.log(max)
 
-function summing(num) {
-    if (num % 2 == 1) {
-        // let num1 = num;
-        if (num > 0) {
-            let i = 0;
-            let num2 = 0;
-            while (i < num) {
-                num2 += num2 + 1;
-                if (i == num) {
-                    break
-                }
-            }
-            return num2
-        } else {
-            return 0;
+// function summing(num) {
+//     if (num % 2 == 1) {
+//         // let num1 = num;
+//         if (num > 0) {
+//             let i = 0;
+//             let num2 = 0;
+//             while (i < num) {
+//                 num2 += num2 + 1;
+//                 if (i == num) {
+//                     break
+//                 }
+//             }
+//             return num2
+//         } else {
+//             return 0;
 
+//         }
+//     } else {
+//         console.log("this number is not Prime number")
+//     }
+// }
+// console.log(summing(5));
+
+/*----------------------------------------------------------------------*/
+/*----------------- Q-1 ------------------------------------------*/
+/*
+function perfect(num) {
+    let i = 1;
+    let res = 0;
+    while (i <= num) {
+        if (num % i == 0) {
+                res +=i
+            }
+            i++;
+        if (num == i) {
+            break;
         }
-    } else {
-        console.log("this number is not Prime number")
     }
+    
+    
+    if (res == num) {
+        return `${num} is a perfect number`
+    } else {
+        `${num} is not a perfect number`
+    }
+    return res
+    }
+
+
+console.log(perfect(5));
+*/
+
+/*----------------------------------------------------------------------*/
+/*----------------- Q-2 ------------------------------------------*/
+/*
+function isPlaindrom(num) {
+    if(num.math)
 }
-console.log(summing(5));
+
+    */
+
+/*-------------------------------------------------------------------*/
+/*-------------------Q-3---------------------------------------------*/
+/*
+function difirent(arr) {
+    let max = arr[0];
+    let min = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max); {
+            max = arr[i]
+
+        } if (arr[i] < min) {
+            min = arr[i]
+        } 
+    } 
+    return max - min
+}
+let arr = [1, 3, 9, 5];
+console.log(difirent(arr));
+// difirent(arr);
+*/
+
+/*---------------------------------------------------------------------*/
+/*-------------- Q-3 --------------------------------------------------*/
+/*
+function changeTheLetter(str, cha) {
+  let arr = ["a", "e", "i", "u", "o"];
+  let res = "";
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < arr.length; j++)
+      if (str[i] == arr[j]) {
+          res += cha;
+          break
+      } else {
+          res += str[i];
+          break
+      }
+  }
+  return res;
+}
+console.log(changeTheLetter("amjad", "*"));
+*/
+
+/*-----------------------------------------------------------------*/
+/*------------Q-4-------------------------------------*/
+/*
+function isVowle(str) {
+  let arr = ["a", "e", "i", "u", "o"];
+//   let res ;
+    for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < arr.length; j++)
+        if (str[i] === arr[j]) {
+            // res += str[i];
+            return i 
+            // break
+            } 
+      
+  }
+//   return res;
+}
+console.log(isVowle("amjad"));
+*/
+
+
+
+/*---------------------------------------------------------------*/
+/*-------------------Q-5-----------------------------------------*/
+// function counter(str, num) {
+//     let arr = ["a", "e", "i", "u", "o"];
+
+// }
+// console.log(counter("amjad", 2));
